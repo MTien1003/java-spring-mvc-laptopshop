f209dfb5584ccd3121f587942743fbfbcf4b8066
MTien1003 <tienbale10@gmail.com>
2025-07-17 02:49:27 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -3,11 +3,23 @@ package vn.hoidanit.laptopshop;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class HelloController {
     @GetMapping("/")
 	public String index() {
-		return "Xin chao cac ban";
+		return "Xin chao";
 	}
+
+	@GetMapping("/user")
+	public String userPage() {
+		return "Only user can see this";
+	}
+
+	@GetMapping("/admin")
+	public String adminPage() {
+		return "Only admin can see this";
+	}
+	
 
 }
